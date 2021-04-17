7a6e248769caa53475d43b7f8c5924e25179305d
KimYongKwon <dydrnjs518@gmail.com>
2021-04-17 18:59:25 +0900

    - Add OrderApp main code

---
+++ src/main/java/spp/core/OrderApp.java
@@ -0,0 +1,24 @@
+package spp.core;
+
+import spp.core.member.AdminMember;
+import spp.core.member.Member;
+import spp.core.memberService.MemberService;
+import spp.core.memberService.MemberServiceImpl;
+import spp.core.orderService.OrderService;
+import spp.core.orderService.OrderServiceImpl;
+import spp.core.repository.MemberRepository;
+import spp.core.repository.MemoryMemberRepository;
+
+public class OrderApp {
+
+    public static void main(String[] args) {
+        MemberRepository memberRepository = new MemoryMemberRepository();
+        MemberService memberService = new MemberServiceImpl();
+        OrderService orderService = new OrderServiceImpl();
+
+        Member member = new AdminMember(2321, "KKK");
+        memberService.join(member);
+
+        orderService.order(member.getId());
+    }
+}
